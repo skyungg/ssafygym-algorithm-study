8c4ca0480d3a378f82eb28cb5229c6970fd02909
skyungg <167261107+skyungg@users.noreply.github.com>
2025-09-15 19:32:45 +0900

    Update and rename BJ_12100_2048(easy).java to BJ_12100_2048(easy)_허승경.java

--- 9월2주차/2048(Easy)/BJ_12100_2048(easy)_허승경.java
+++ 9월2주차/2048(Easy)/BJ_12100_2048(easy)_허승경.java
@@ -1,3 +1,8 @@
+/*
+ * 아이디어: 그냥 타고타고 구현하기
+ * 
+ * */
+
 import java.io.*;
 import java.util.*;
 
@@ -16,35 +21,36 @@ public class Main {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
-                maxResult = Math.max(maxResult, map[i][j]);
+                maxResult = Math.max(maxResult, map[i][j]);		// 현재 가장 큰 블록값으로 값 초기화
             }
         }
 
         curMap = new int[N][N];
-        copyMap();
-        start(0);
-        System.out.println(maxResult);
+        copyMap();		// 초기 상황을 복사
+        start(0);		// dfs 시작
+        System.out.println(maxResult);		// 결과 출력
     }
 
     static void start(int cnt) {
-        if (cnt == 5) return;
+        if (cnt == 5) return;		// 최대 5번 까지 가능하니까
 
         int[][] backup = new int[N][N];
         for (int i = 0; i < N; i++) backup[i] = curMap[i].clone();
 
-        moveUp();
+        // 4방향으로 각각 이동하기 
+        moveUp();			// 1. 위쪽으로 올려
+        start(cnt + 1);
+        restore(backup);	// 다시 복구하기
+
+        moveDown();			// 2. 아래로 내리기
+        start(cnt + 1);
+        restore(backup);
+        	
+        moveLeft();			// 3. 왼쪽으로
         start(cnt + 1);
         restore(backup);
 
-        moveDown();
-        start(cnt + 1);
-        restore(backup);
-
-        moveLeft();
-        start(cnt + 1);
-        restore(backup);
-
-        moveRight();
+        moveRight();		// 4. 오른쪽으로 
         start(cnt + 1);
         restore(backup);
     }
@@ -54,24 +60,27 @@ public class Main {
     }
 
     static void moveUp() {
+    	// 위쪽으로 블록 다 올려버리기
+    	
         for (int j = 0; j < N; j++) {
-            int[] temp = new int[N];
+            int[] temp = new int[N];		// 합쳐진 후 결과 저장
             int idx = 0, prev = 0;
             for (int i = 0; i < N; i++) {
-                if (curMap[i][j] == 0) continue;
-                if (prev == 0) {
-                    prev = curMap[i][j];
-                } else if (prev == curMap[i][j]) {
-                    temp[idx++] = prev * 2;
-                    maxResult = Math.max(maxResult, prev * 2);
+                if (curMap[i][j] == 0) continue;	// 현재 빈칸은 넘어가기
+                if (prev == 0) {					// 비어 있음 -> 현재값을 직전값으로 저장하기
+                    prev = curMap[i][j];		// 
+                } else if (prev == curMap[i][j]) {	// 현재값과 직전값이 같음 0> 합치고 직전값은 0
+                    temp[idx++] = prev * 2;		// 합쳐진 값 넣기
+                    maxResult = Math.max(maxResult, prev * 2);	// 최대 블록 값 갱신
                     prev = 0;
                 } else {
-                    temp[idx++] = prev;
-                    prev = curMap[i][j];
+                    temp[idx++] = prev;	// 직전값 저장
+                    prev = curMap[i][j];	// 직전값 갱신	
                 }
             }
-            if (prev != 0) temp[idx] = prev;
-            for (int i = 0; i < N; i++) curMap[i][j] = temp[i];
+            
+            if (prev != 0) temp[idx] = prev;	// 남은 직전값 temp에 넣기
+            for (int i = 0; i < N; i++) curMap[i][j] = temp[i];		// 최종 결과 반영
         }
     }
 
